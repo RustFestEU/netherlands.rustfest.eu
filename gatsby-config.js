f401d79a819426c2945ade79d3eaa26b03c86548
Alexander Varwijk <git@alexandervarwijk.com>
2020-01-19 17:34:24 +0100

    Add TailwindCSS as CSS framework
    We use a framework to increase the speed of development. Tailwind was
    selected because it's utility based philosophy gets out of the way of
    getting things done.
    
    We begin by cleaning up the logo to be a reusable component and avoiding
    all that SVG in the index page file.

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,7 @@ module.exports = {
         ],
       },
     },
+    `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,7 +9,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import { useStaticQuery, graphql } from 'gatsby';
 
-import './layout.css';
+import '../styles/index.css';
 
 const Layout = ({ children, isFront = false }) => {
   // const data = useStaticQuery(graphql`
